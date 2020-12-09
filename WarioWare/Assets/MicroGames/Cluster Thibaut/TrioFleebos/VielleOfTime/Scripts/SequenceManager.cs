d4ec5777d9b9152f4322011216bf62504bc94669
Arthur Spellani <a.spellani@rubika-edu.com>
2020-12-09 17:08:50 +0100

    Vielle Turn & Sequence

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioFleebos/VielleOfTime/Scripts/SequenceManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testing;
+
+namespace Fleebos
+{
+    namespace VielleOfTime
+    {
+        /// <summary>
+        /// Arthur SPELLANI
+        /// </summary>
+
+        public class SequenceManager : TimedBehaviour
+        {
+            public GameObject particleWin;
+
+            public GameObject[] Inputs;
+            public GameObject[] NotesUI;
+            public List<GameObject> MusicNotes;
+            
+            int ListOfNotesLength;
+            int Easy = 3;
+            int Medium = 4;
+            int Hard = 5;
+
+            int NoteOrder = 0;
+
+            VielleManager VL;
+
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+                VL = GetComponent<VielleManager>();
+                ListOfNotesLength = Hard;
+
+
+                /*switch (currentDifficulty)
+                {
+                    case Manager.Difficulty.EASY:
+                        ListOfNotesLength = Easy;
+                        break;
+
+                    case Manager.Difficulty.MEDIUM:
+                        ListOfNotesLength = Medium;
+                        break;
+
+                    case Manager.Difficulty.HARD:
+                        ListOfNotesLength = Hard;
+                        break;
+                }*/
+
+                for (int i = 0; i < ListOfNotesLength; i++)
+                {
+                    MusicNotes.Add(Inputs[Random.Range(0, 4)]);
+                }
+
+                for (int i = 0; i < MusicNotes.Count; i++)
+                {
+                    Instantiate(MusicNotes[i], NotesUI[i].transform);
+                }
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+                if (VL.turnActivation == true)
+                {
+                    if (Input.GetButtonDown("A_Button"))
+                    {
+                        if(MusicNotes[NoteOrder].name == "A_Button_Sequence")
+                        {
+                            Instantiate(particleWin, NotesUI[NoteOrder].transform);
+                            NoteOrder++;                            
+                        }
+                    }
+                    if (Input.GetButtonDown("B_Button"))
+                    {
+                        if (MusicNotes[NoteOrder].name == "B_Button_Sequence")
+                        {
+                            Instantiate(particleWin, NotesUI[NoteOrder].transform);
+                            NoteOrder++;
+                        }
+                    }
+                    if (Input.GetButtonDown("X_Button"))
+                    {
+                        if (MusicNotes[NoteOrder].name == "X_Button_Sequence")
+                        {
+                            Instantiate(particleWin, NotesUI[NoteOrder].transform);
+                            NoteOrder++;
+                        }
+                    }
+                    if (Input.GetButtonDown("Y_Button"))
+                    {
+                        if (MusicNotes[NoteOrder].name == "Y_Button_Sequence")
+                        {
+                            Instantiate(particleWin, NotesUI[NoteOrder].transform);
+                            NoteOrder++;
+                        }
+                    }
+                }
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+            }
+
+            public void PlayNote()
+            {
+                
+            }
+        }
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioFleebos/VielleOfTime/Scripts/VielleManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fleebos
+{
+    namespace VielleOfTime
+    {
+        /// <summary>
+        /// Arthur SPELLANI
+        /// </summary>
+        
+        public class VielleManager : TimedBehaviour
+        {
+            Vector2 joystickRotation;
+            Vector2 currentJoystickRotation;
+
+            public Image turnSignal;
+
+            [HideInInspector] public bool turnActivation = false;
+
+
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+                joystickRotation = new Vector2(1,1);
+                turnSignal.color = Color.red;
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+                currentJoystickRotation = new Vector2(Input.GetAxis("Left_Joystick_X"), Input.GetAxis("Left_Joystick_Y"));
+
+                if (turnSignal.fillAmount > 0)
+                {
+                    turnSignal.fillAmount -= Time.deltaTime / 2;
+                }      
+                
+                if(turnSignal.fillAmount > 0.85f && turnActivation == false)
+                {
+                    TurnIsOn();
+                }
+                if (turnSignal.fillAmount < 0.85f && turnActivation == true)
+                {
+                    TurnIsOff();
+                }
+
+                if (Vector2.Angle(currentJoystickRotation, joystickRotation) > 45 && Vector2.Angle(currentJoystickRotation, joystickRotation) < 100)
+                {
+                    turnSignal.fillAmount += 0.1f;
+                    joystickRotation = currentJoystickRotation;
+                }
+
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+            }
+
+            public void TurnIsOn()
+            {
+                turnActivation = true;
+                turnSignal.color = Color.green;
+            }
+
+            public void TurnIsOff()
+            {
+                turnActivation = false;
+                turnSignal.color = Color.red;
+            }
+        }
+    }
+}
